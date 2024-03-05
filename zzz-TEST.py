cf12628ca02d119d23c043b07f6f09d5e877f5e3
alesha-uwu <leona.pierce@proton.me>
2024-03-05 00:03:28 -0500

    update

--- zzz-TEST.py
+++ zzz-TEST.py
@@ -1,15 +1,8 @@
-def test(d):
-    seen = []
-    inverted = {}
-    for i in d:
-        if d[i] in seen:
-            pass
-        else:
-            if d[i] in inverted:
-                del inverted[d[i]]
-                seen.append(d[i])
-            else:
-                inverted[d[i]] = i
+def test(trie):
+    word = "ABC"
+    word.lower()
 
-    return inverted
-print(test({'one':1, 'two':2, 'uno':1, 'dos':2, 'three':3}))
+    return word
+    
+
+print(test('words.txt'))
